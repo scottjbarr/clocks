d589f256122bef611f3615d15aa46a03c6b05f06
Scott Barr <scottjbarr@gmail.com>
2021-01-20 20:33:54 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Clocks
+
+Print a bunch of clocks side by side.
+
+Loops forever, clearing the terminal and printing times.
+
+## Usage
+
+```
+$ clocks -tz Australia/Sydney,Asia/Tokyo,Europe/London,America/New_York
++--------------------+------------+---------------+------------------+
+| AUSTRALIA/SYDNEY   | ASIA/TOKYO | EUROPE/LONDON | AMERICA/NEW YORK |
++--------------------+------------+---------------+------------------+
+|           21:30:47 |   19:30:47 |      10:30:47 |         05:30:47 |
++--------------------+------------+---------------+------------------+
+```
+
+## Licence
+
+The MIT License (MIT)
+
+Copyright (c) 2021 Scott Barr
+
+See [LICENSE](LICENSE)
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/olekukonko/tablewriter"
+)
+
+func main() {
+	tz := ""
+	layout := ""
+
+	flag.StringVar(&tz, "tz", "", "Australia/Sydney,Asia/Tokyo,Europe/London,America/New_York")
+	flag.StringVar(&layout, "layout", "horizontal", "Layout horizontal or vertical.")
+	flag.Parse()
+
+	if len(tz) == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	zones := strings.Split(tz, ",")
+
+	locations := make([]*time.Location, len(zones), len(zones))
+
+	for i, zone := range zones {
+		loc, err := time.LoadLocation(zone)
+		if err != nil {
+			panic(err)
+		}
+
+		locations[i] = loc
+	}
+
+	if layout == "vertical" {
+		vertical(zones, locations)
+		return
+	}
+
+	horizontal(zones, locations)
+}
+
+func horizontal(zones []string, locations []*time.Location) {
+	for {
+		// clear
+		fmt.Print("\033[H\033[2J")
+
+		// setup a table writer
+		w := tablewriter.NewWriter(os.Stdout)
+		w.SetHeader(zones)
+		w.SetAlignment(tablewriter.ALIGN_RIGHT)
+
+		// create the times for each timezone
+		data := []string{}
+
+		n := time.Now()
+
+		for _, loc := range locations {
+			data = append(data, n.In(loc).Format("15:04:05"))
+		}
+
+		// add the times to the table
+		w.AppendBulk([][]string{data})
+
+		// write the table
+		w.Render()
+
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func vertical(zones []string, locations []*time.Location) {
+	// vertical clocks
+	for {
+		// clear
+		fmt.Print("\033[H\033[2J")
+
+		// setup a table writer
+		w := tablewriter.NewWriter(os.Stdout)
+		w.SetAlignment(tablewriter.ALIGN_LEFT)
+
+		// create the times for each timezone
+		lines := [][]string{}
+
+		n := time.Now()
+
+		for i, loc := range locations {
+			line := []string{}
+			line = append(line, zones[i])
+			line = append(line, n.In(loc).Format("15:04:05"))
+
+			lines = append(lines, line)
+		}
+
+		// add the times to the table
+		w.AppendBulk(lines)
+
+		// write the table
+		w.Render()
+
+		time.Sleep(1 * time.Second)
+	}
+}
